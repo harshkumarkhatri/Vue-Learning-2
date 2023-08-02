92ea678ea494d8736afdb51106d423500219dc52
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2023-08-02 14:23:27 +0530

    added form, learnt about binding variable and method with form

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,23 @@
     :index="activePage"
     :update-active-index="(index) => (activePage = index)"
   ></page-navbar>
-  <page-viewer :page="pages[activePage]"></page-viewer>
+  <create-page :page-created="pageCreated"></create-page>
+  <!-- <page-viewer :page="pages[activePage]"></page-viewer>
+   -->
   <!-- First approach of handling when data is being loaded -->
   <!-- v-if="pages.length > 0" -->
 </template>
 
 <script>
-import PageViewer from "./components/PageViewer.vue";
+// import PageViewer from "./components/PageViewer.vue";
 import PageNavbar from "./components/PageNavbar.vue";
+import CreatePage from "./components/CreatePage.vue";
 
 export default {
   components: {
     PageNavbar,
-    PageViewer,
+    // PageViewer,
+    CreatePage,
   },
   created() {
     this.getPages();
@@ -34,6 +38,9 @@ export default {
       let res = await fetch("pages.json");
       let data = await res.json();
       this.pages = data;
+    },
+    pageCreated(pageObject) {
+      console.log(pageObject);
     },
   },
 };
---
+++ src/components/CreatePage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container mb-3">
+    <form action="">
+      <div class="mb-3">
+        <label for="" class="form-label"> Page Title </label>
+        <input type="text" class="form-control" v-model="pageTitle" />
+        <!--
+            First way of setting up binding for a form
+             :value="pageTitle"
+          @input="(e) => (pageTitle = e.target.value)" -->
+      </div>
+      <div class="mb-3">
+        <label for="" class="form-label"> Content </label>
+        <textarea
+          type="text"
+          class="form-control"
+          rows="5"
+          v-model="content"
+        ></textarea>
+      </div>
+      <div class="mb-3">
+        <button
+          class="btn btn-primary"
+          @click="pageCreated({ pageTitle, content })"
+        >
+          Create Page
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreatePage",
+  props: ["pageCreated"],
+  data() {
+    return {
+      pageTitle: "",
+      content: "",
+    };
+  },
+};
+</script>
